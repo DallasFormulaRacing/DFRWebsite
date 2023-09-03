c9b0410793ee1526dfd8c41083670879829b8f7d
bellh14 <haybell965@gmail.com>
2023-09-03 10:17:38 -0500

    update social links

--- dfr-website/app/(shared-components)/Navbar.tsx
+++ dfr-website/app/(shared-components)/Navbar.tsx
@@ -1,36 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import DFRLogo from "@/public/DFR-Logo.png";
 import SocialLinks from "@/app/(shared-components)/SocialLinks";
 import Car2023 from "@/public/assets/car_gallery/2023Car.jpeg";
-1;
+
 type Props = {};
 
 const Navbar = (props: Props) => {
+    const [navbarToggle, setNavbarToggle] = useState<boolean>(false);
+
     return (
+
         <header className={"mb-0"}>
             <nav
                 className={
-                    "flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4"
+                    "flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4 navbar"
                 }
             >
                 <div className={"basis-1/6 relative w-auto h-32"}>
-                    <a href={"/"} rel={"noreferrer"}>
+                    <Link href={"/"} rel={"noreferrer"}>
                         <Image
                             fill
                             alt={"logo"}
                             placeholder={"blur"}
                             src={DFRLogo}
                             sizes={
-                                "(max-width: 480px) 100vw, (max-width: 480px) 100vw, (max-width: 768px) 75vw, (max-width: 1060px) 50vw, 25vw"
+                                "(max-width: 480px) 100dvw, (max-width: 480px) 100dvw, (max-width: 768px) 75dvw, (max-width: 1060px) 50dvw, 25vw"
                             }
                             style={{ objectFit: "scale-down" }}
                         />
-                    </a>
-                </div>
-                <div className={"hidden sm:block"}>
-                    <SocialLinks />
+                    </Link>
                 </div>
                 <div className={"flex justify-between items-center gap-10"}>
                     <Link href={"/about"}>About</Link>
@@ -38,9 +39,10 @@ const Navbar = (props: Props) => {
                     <Link href={"/team"}>Team</Link>
                     <Link href={"/blog"}>News</Link>
                     <Link href={"/sponsors"}>Sponsors</Link>
+                    <Link href={"/contact"}>Contact</Link>
                 </div>
                 <div>
-                    <p>Sign In</p>
+                    <SocialLinks />
                 </div>
             </nav>
             <hr className={"border-0"} />
--- dfr-website/app/(shared-components)/SocialLinks.tsx
+++ dfr-website/app/(shared-components)/SocialLinks.tsx
@@ -5,6 +5,8 @@ import Facebook from "/public/assets/social_links/social_facebook.png";
 import Instagram from "/public/assets/social_links/social_instagram.png";
 import Google from "/public/assets/social_links/social_google.png";
 import Discord from "/public/assets/social_links/social_discord.png";
+import Linkedin from "/public/assets/social_links/social_linkedin.png";
+import Youtube from "/public/assets/social_links/social_youtube.jpg";
 
 type Props = {
   isDark?: boolean;
@@ -13,24 +15,16 @@ type Props = {
 const SocialLinks = ({ isDark = false }: Props) => {
   return (
     <div className={"flex justify-between items-center gap-7"}>
-      <a href={"https://twitter.com"} target={"_blank"} rel={"noreferrer"}>
+      <a href={"https://youtube.com"} target={"_blank"} rel={"noreferrer"}>
         <Image
           className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
-          alt={"twitter"}
-          src={Twitter}
-          width={20}
-          height={20}
+          alt={"youtube"}
+          src={Youtube}
+          width={30}
+          height={30}
         />
       </a>
-      <a href={"https://facebook.com"} target={"_blank"} rel={"noreferrer"}>
-        <Image
-          className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
-          alt={"facebook"}
-          src={Facebook}
-          width={20}
-          height={20}
-        />
-      </a>
+      
       <a href={"https://instagram.com"} target={"_blank"} rel={"noreferrer"}>
         <Image
           className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
@@ -40,15 +34,17 @@ const SocialLinks = ({ isDark = false }: Props) => {
           height={20}
         />
       </a>
-      <a href={"https://google.com"} target={"_blank"} rel={"noreferrer"}>
+
+      <a href={"https://linkedin.com"} target={"_blank"} rel={"noreferrer"}>
         <Image
           className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
-          alt={"google"}
-          src={Google}
+          alt={"linkedin"}
+          src={Linkedin}
           width={20}
           height={20}
         />
       </a>
+      
       <a href={"https://discord.com"} target={"_blank"} rel={"noreferrer"}>
         <Image
           className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
