4a69a921ee0066b5b7fa2209717f95e8dd251b86
bellh14 <haybell965@gmail.com>
2023-09-26 16:41:55 -0500

    fixed unused blog for now

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,47 +1,47 @@
-import React from "react";
-//import sidebar
-import { prisma } from "@/app/api/client";
-import { User as UserType, Post as PostType } from "@prisma/client";
-import { FormattedPost } from "@/app/types";
-import Content from "./Content";
+// import React from "react";
+// //import sidebar
+// import { prisma } from "@/app/api/client";
+// import { User as UserType, Post as PostType } from "@prisma/client";
+// import { FormattedPost } from "@/app/types";
+// import Content from "./Content";
 
-type Props = {
-  params: { id: string };
-};
+// type Props = {
+//   params: { id: string };
+// };
 
-const getPost = async (id: string) => {
-  const post: PostType | null = await prisma.post.findUnique({
-    where: { id },
-  });
+// const getPost = async (id: string) => {
+//   const post: PostType | null = await prisma.post.findUnique({
+//     where: { id },
+//   });
 
-  if (!post) {
-    console.log(`Post with id ${id} not found`);
-    return null;
-  }
+//   if (!post) {
+//     console.log(`Post with id ${id} not found`);
+//     return {id: "", title: "", content: ""};
+//   }
 
-  const formattedPost = {
-    ...post,
-    createdAt: post?.createdAt?.toISOString(),
-    updatedAt: post?.updatedAt?.toISOString(),
-  };
+//   const formattedPost = {
+//     ...post,
+//     createdAt: post?.createdAt?.toISOString(),
+//     updatedAt: post?.updatedAt?.toISOString(),
+//   };
 
-  return formattedPost;
-};
+//   return formattedPost;
+// };
 
-const Post = async ({ params }: Props) => {
-  const { id } = params;
-  const post: FormattedPost | null = await getPost(id);
+// const Post = async ({ params }: Props) => {
+//   const { id } = params;
+//   const post: FormattedPost | null = await getPost(id);
 
-  return (
-    <main className={"px-10 leading-7"}>
-      <div className={"md:flex gap-10 mb-5"}>
-        <div className={"basis-3/4"}>
-          <Content post={post} />
-        </div>
-        <div className={"basis-1/4"}>{/*<Sidebar />*/}</div>
-      </div>
-    </main>
-  );
-};
+//   return (
+//     <main className={"px-10 leading-7"}>
+//       <div className={"md:flex gap-10 mb-5"}>
+//         <div className={"basis-3/4"}>
+//           <Content post={post} />
+//         </div>
+//         <div className={"basis-1/4"}>{/*<Sidebar />*/}</div>
+//       </div>
+//     </main>
+//   );
+// };
 
-export default Post;
+// export default Post;
