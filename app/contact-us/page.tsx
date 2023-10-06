628b86f76b65801e68f5a61cc7dc8710f8095102
Fernando Portillo <fernport@Fernandos-MacBook-Air.local>
2023-10-06 13:26:59 -0500

    set up look of page

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { Space, Form, Input } from "antd";
+const { TextArea } = Input;
+
+type FormValues = {
+    name: string;
+    email: string;
+    message: string;
+};
+
+export default function Page() {
+    return (
+        <main className="min-h-full bg-wh-900 flex flex-col justify-center items-center text-white text-center p-4 md:p-48">
+            <Space direction="vertical" size="large" className="min-h-1/2">
+                <h1 className="text-5xl sm:text-8xl font-bold">Get in Touch</h1>
+                <p className="text-xl">
+                    Want to learn more about Dallas Formula Racing? Interested
+                    in becoming a sponsor? We would love to hear from you!
+                </p>
+
+                <p className="text-xl">Visit: ECSW 1.455</p>
+                <p className="text-xl">Email: dallasformularacing@gmail.com</p>
+            </Space>
+
+            <Form className="w-full mt-16">
+                <h2 className="text-xl font-bold text-white my-8">
+                    Message Us
+                </h2>
+                <div className="flex flex-col md:gap-24 md:flex-row">
+                    <Form.Item
+                        className="w-full md:w-1/2"
+                        name="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your name",
+                            },
+                        ]}
+                    >
+                        <Input className="p-4" placeholder="Name" />
+                    </Form.Item>
+                    <Form.Item
+                        className="w-full md:w-1/2"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your name",
+                            },
+                        ]}
+                    >
+                        <Input
+                            className="p-4"
+                            placeholder="Email"
+                            type="email"
+                        />
+                    </Form.Item>
+                </div>
+                <Form.Item name="message">
+                    <TextArea rows={8} allowClear placeholder="Message" />
+                </Form.Item>
+                <button className="bg-accent-green text-wh-0 text-xl font-semibold w-40 h-12 rounded-md mt-8">
+                    Send
+                </button>
+            </Form>
+        </main>
+    );
+}
