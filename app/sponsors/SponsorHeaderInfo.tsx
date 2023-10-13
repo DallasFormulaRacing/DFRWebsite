dcb7bd4903945ae06e33478b99d40c0d783ac83d
Fernando Portillo <fernport@Fernandos-MacBook-Air.local>
2023-10-13 14:11:56 -0500

    small fix to header info on sponsors

--- app/sponsors/SponsorHeaderInfo.tsx
+++ app/sponsors/SponsorHeaderInfo.tsx
@@ -10,30 +10,31 @@ export const SponsorHeaderInfo = (props: props) => {
             <ImageBreaks
                 image={Car2023.src}
                 imageHeight={"h-144"}
-                imageFit={"object-fill"}
+                imageFit={"object-cover"}
                 imageBrightness={"brightness-50"}
             />
-            <div className={"absolute left-1/3 bottom-1/2"}>
+            <div className={"absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-1/2 text-center space-y-8"}>
                 <h1
                     className={
-                        "sm:text-5xl md:text-8xl lg:text-10xl font-bold text-wh-75"
+                        "text-6xl md:text-7xl font-bold text-wh-75"
                     }
                 >
                     Sponsors
                 </h1>
-            </div>
-            <div className={"absolute mx-40 bottom-20"}>
-                <p className={"text-wh-75 text-lg font-semibold"}>
-                    Dallas Formula Racing is a student-led organization that
-                    relies on the generosity of our sponsors and donors. We are
-                    thankful to all of our supporters for helping our team
-                    further develop the education of each student on our team.
-                    Our supporters hold a special place in our heart and we
-                    would like to acknowledge them below. We are proud to
-                    represent our supporters. If you would like to make a
-                    donation or become a sponsor contact us at
-                    DallasFormulaRacing@gmail.com
-                </p>
+
+                <div className={""}>
+                    <p className={"text-wh-75 text-md md:text-lg font-semibold"}>
+                        Dallas Formula Racing is a student-led organization that
+                        relies on the generosity of our sponsors and donors. We
+                        are thankful to all of our supporters for helping our
+                        team further develop the education of each student on
+                        our team. Our supporters hold a special place in our
+                        heart and we would like to acknowledge them below. We
+                        are proud to represent our supporters. If you would like
+                        to make a donation or become a sponsor contact us at
+                        DallasFormulaRacing@gmail.com
+                    </p>
+                </div>
             </div>
         </div>
     );
