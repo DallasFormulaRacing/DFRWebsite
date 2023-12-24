af4f0b48c44ccc61f538f018ebe3613ecf1fd91c
bellh14 <haybell965@gmail.com>
2023-12-23 23:29:48 -0600

    template db and models

--- Backend/go.mod
+++ Backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/DallasFormulaRacing/DFRWebsite
 
 go 1.21.5
+
+require github.com/lib/pq v1.10.9 // indirect
--- Backend/pkg/types/types.go
+++ Backend/pkg/types/types.go
@@ -1,1 +1,51 @@
 package types
+
+import (
+	"time"
+)
+
+type Account struct {
+	ID        string    `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Posistion string    `json:"position"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type Post struct {
+	ID        string    `json:"id"`
+	Author    string    `json:"author"`
+	Title     string    `json:"title"`
+	Tags      []string  `json:"tags"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type FormattedPost struct {
+	ID        string    `json:"id"`
+	Author    string    `json:"author"`
+	Title     string    `json:"title"`
+	Tags      []string  `json:"tags"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Image     string    `json:"image"`
+	Snippet   string    `json:"snippet"`
+}
+
+type Event struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Location  string    `json:"location"`
+	StartTime time.Time `json:"startTime"`
+	EndTime   time.Time `json:"endTime"`
+	Link      string    `json:"link"`
+}
+
+type Sponsor struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Logo string `json:"logo"`
+	Link string `json:"link"`
+}
