43ebfca584d0090e0caed85c424137a97ecb25c4
bellh14 <haybell965@gmail.com>
2023-12-26 23:09:41 -0600

    start post api fix npm i failures

--- app/post/PostSlice.tsx
+++ app/post/PostSlice.tsx
@@ -1,3 +1,43 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import produce from "immer";
 import { RootState } from "../api/store"
+
+
+export enum PostStatuses {
+    Initial = "Not Feteched",
+    Loading = "Loading",
+    UpToDate = "Up To Date",
+    Deleted = "Deleted",
+    Error = "Error",
+}
+
+export interface PostState {
+    id?: number;
+    title?: string;
+    author?: string;
+    content?: string;
+    created_at?: any;
+    updated_at?: any;
+    image?: string;
+    snippet?: string;
+    tags?: string[];
+}
+
+export interface PostFormData {
+    post: {
+        id?: number;
+        title: string;
+        content: string;
+    };
+}
+
+export interface PostsState {
+    posts: PostState[];
+    status: string;
+}
+
+export interface PostDeleteData {
+    post: {
+        post_id: number;
+    };
+}
