53c29912c6cfb82a445bd22f7ab87f5c5ab8c118
bellh14 <haybell965@gmail.com>
2024-01-16 20:14:31 -0600

    add lead bio header as card

--- app/team/ic/(components)/LeadHeader.tsx
+++ app/team/ic/(components)/LeadHeader.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import OfficerList from "../../officers/OfficerData";
 
 type LeadHeaderProps = {
     LeadName: string;
@@ -10,15 +9,15 @@ type LeadHeaderProps = {
 
 const LeadHeader = (props: LeadHeaderProps) => {
     return (
-        <div className={"bg-black w-3/4 h-1/2 my-10"}>
+        <div className={"w-3/4 h-1/2 my-10"}>
             <div className={"items-center justify-center py-8"}>
-                <h1 className={"text-white text-3xl mx-4 my-4"}>
+                <h1 className={"text-3xl mx-4 my-4"}>
                     {props.LeadName}
                 </h1>
-                <h2 className={"text-white text-2xl mx-10 my-8"}>
+                <h2 className={"text-2xl mx-10 my-8"}>
                 {props.SubTeam} Lead
                 </h2>
-                <p className={"text-white text-xl mx-10"}>
+                <p className={"text-xl mx-10"}>
                     {props.LeadBio}
                 </p>
             </div>
--- app/team/ic/(subteams)/aerodynamics/page.tsx
+++ app/team/ic/(subteams)/aerodynamics/page.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import SubTeamHeader from "../../(components)/SubTeamHeader";
+import LeadHeader from "../../(components)/LeadHeader";
+import { Col, Row, Card } from "antd";
+import OfficerList from "../../../officers/OfficerData";
+import Image from "next/image";
+import { Officer, OfficerData } from "@/app/types";
+import BlankAvatar from "/public/assets/team/Headshots/blank_avatar.png";
 
 type Props = {};
 
+const { Meta } = Card;
+
 export default function Aerodynamics() {
+    const [officerList, setOfficerList] = useState<OfficerData>(OfficerList);
     return (
-        <main className="tracking-wide h-full">
+        <main className="tracking-wide my-4">
             <SubTeamHeader
                 Subteam="Aerodynamics"
                 Meetings="TBD"
@@ -14,6 +24,61 @@ export default function Aerodynamics() {
                 focus will be on downforce devices, bodywork drag reduction, 
                 and cooling."
             />
+
+            {/* <Row>
+                <Col span={12}>
+                    <Image
+                        src={
+                            officerList.Engineering.data[0].image
+                                ? officerList.Engineering.data[0].image
+                                : BlankAvatar
+                        }
+                        alt="Aerodynamics Lead"
+                        fill
+                        className="rounded-lg object-contain w-full md:ml-18 lg:ml-24"
+                    />
+                </Col>
+                <Col span={12}>
+                    <LeadHeader
+                        LeadName={officerList.Engineering.data[0].name}
+                        SubTeam="Aerodynamics"
+                        LeadBio="This team has been the perfect opportunity 
+                        for me to explore my passion for motorsport engineering 
+                        and has let me find a niche at UTD that I deeply care about. 
+                        This team has let me teach other passionate individuals about 
+                        topics I care about, which is something I’m very grateful for. 
+                        I hope I can continue this process of learning and teaching for 
+                        future members to continue to benefit from."
+                        // Email={officerList.Engineering.data[0].email} cannot be null email
+                        Email="rrh200002@utdallas.edu"
+                    />
+                </Col>
+                      </Row> */}
+            <div className="flex justify-center">
+                <Card
+                    className="flex flex-col text-center w-[400px] md:w-[750px] mx-auto my-8"
+                    hoverable
+                    cover={
+                        <img
+                            src={officerList.Engineering.data[0].image}
+                            alt="Aerodynamics Lead"
+                            className="object-contain py-4"
+                            style={{ height: "200px" }}
+                        />
+                    }
+                >
+                    <Meta
+                        title={officerList.Engineering.data[0].name}
+                        description="This team has been the perfect opportunity for 
+                    me to explore my passion for motorsport engineering and has 
+                    let me find a niche at UTD that I deeply care about. This team 
+                    has let me teach other passionate individuals about topics I care 
+                    about, which is something I’m very grateful for. I hope I can continue 
+                    this process of learning and teaching for future members to continue to 
+                    benefit from."
+                    />
+                </Card>
+            </div>
         </main>
     );
 }
