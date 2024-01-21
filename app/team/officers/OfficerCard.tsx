c1b4951337656dd0b330afcfc6fb76c7d4342749
bellh14 <haybell965@gmail.com>
2024-01-20 19:57:21 -0600

    add requested minors to major section

--- app/team/officers/OfficerCard.tsx
+++ app/team/officers/OfficerCard.tsx
@@ -12,27 +12,29 @@ const OfficerCard = ({ officer }: { officer: Officer }) => {
     return (
         <div
             className={
-                "my-4 mx-8 col-span-auto flex flex-col justify-end items-center w-[620px] h-[600px]"
+                "my-4 mx-8 col-span-auto flex flex-col justify-end items-center w-[635px] h-[600px]"
             }
         >
-                <Image
-                    src={officer.image ? officer.image : BlankAvatar}
-                    alt={officer.name}
-                    width={225}
-                    height={225}
-                    style={{ clipPath: "circle()", objectFit: "cover" }}
-                    className={"h-full"}
-                    loading="eager"
-                    placeholder={"blur"}
-                    blurDataURL="/assets/team/Headshots/blank_avatar.png"
-                />
+            <Image
+                src={officer.image ? officer.image : BlankAvatar}
+                alt={officer.name}
+                width={225}
+                height={225}
+                style={{ clipPath: "circle()", objectFit: "cover" }}
+                className={"h-full"}
+                loading="eager"
+                placeholder={"blur"}
+                blurDataURL="/assets/team/Headshots/blank_avatar.png"
+            />
 
             <div
                 className={
                     "bg-accent-orange-light shadow-lg rounded-xl z-10 w-1/2 text-center h-[400px]"
                 }
             >
-                <h1 className={"card-title text-3xl text-center font-bold mt-2"}>
+                <h1
+                    className={"card-title text-3xl text-center font-bold mt-2"}
+                >
                     {officer.name}
                 </h1>
                 <h2 className={"card-subtitle text-center text-2xl"}>
