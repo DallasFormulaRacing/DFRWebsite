7751ac5c51d8de774842fe80f4cef16ac79578da
bellh14 <haybell965@gmail.com>
2024-01-23 17:49:28 -0600

    update list of sponsors and logos

--- app/sponsors/SponsorComponent.tsx
+++ app/sponsors/SponsorComponent.tsx
@@ -10,8 +10,8 @@ export const Sponsors = (props: Props) => {
         <div className={"py-24 md:mx-40"}>
             <h1 className={"text-5xl font-bold text-center mb-12"}>Sponsors</h1>
             <div className={"grid grid-cols-3 md:grid-cols-5"}>
-                {Array.from(Array(27).keys()).map((index) => ( 
-                    <div className="col-span-auto flex justify-center items-center"> 
+                {Array.from(Array(33).keys()).map((index) => (
+                    <div className="col-span-auto flex justify-center items-center">
                         <SponsorIcon index={index} />
                     </div>
                 ))}
--- app/sponsors/SponsorData.ts
+++ app/sponsors/SponsorData.ts
@@ -18,37 +18,54 @@ export const SponsorData: Array<Sponsor> = [
         logo: "/assets/sponsors/allamb.webp",
         link: "https://dallashonda.com/",
     },
-    
+
     {
         name: "Altium Design",
         logo: "/assets/sponsors/altium.webp",
         link: "https://www.altium.com/",
     },
-    
+
     {
         name: "ANSYS",
         logo: "/assets/sponsors/ansys.svg",
         link: "https://www.ansys.com/",
     },
-    
+    {
+        name: "B9 Creations",
+        logo: "/assets/sponsors/b9.png",
+        link: "https://www.b9c.com/",
+    },
+
+    {
+        name: "Brembo",
+        logo: "/assets/sponsors/Brembo-logo.png",
+        link: "https://www.brembo.com/en",
+    },
+
     {
         name: "CONVERGE CFD",
         logo: "/assets/sponsors/converge.svg",
         link: "https://convergecfd.com/",
     },
-    
+
+    {
+        name: "DeatschWerks",
+        logo: "/assets/sponsors/Deatschwerks-logo.png",
+        link: "https://www.deatschwerks.com/",
+    },
+
     {
         name: "Evan Composites",
-        logo: "/assets/sponsors/evancomposites.webp",
+        logo: "/assets/sponsors/EvansComposites.png",
         link: "https://www.evancomposites.com/",
     },
-    
+
     {
         name: "Fibre Glast",
         logo: "/assets/sponsors/fibreglast.webp",
         link: "https://www.fibreglast.com/",
     },
-    
+
     {
         name: "HEXAGON",
         logo: "/assets/sponsors/hexagon.svg",
@@ -60,34 +77,34 @@ export const SponsorData: Array<Sponsor> = [
         logo: "/assets/sponsors/labjack.webp",
         link: "https://labjack.com/",
     },
-    
+
     {
         name: "Liqui Moly",
         logo: "/assets/sponsors/liquimoly.svg",
         link: "https://www.liqui-moly.com",
     },
-    
+
     {
         name: "Micro Measurements",
-        logo: "/assets/sponsors/micromeasurements.svg",
+        logo: "/assets/sponsors/micromeasurements.png",
         link: "https://micro-measurements.com/",
     },
-    
+
     {
         name: "NTBAA",
-        logo: "/assets/sponsors/ntbaa.svg",
+        logo: "/assets/sponsors/ntbaa.png",
         link: "https://ntbaaonline.com/",
     },
-    
+
     {
         name: "Peterbilt",
         logo: "/assets/sponsors/peterbilt.webp",
         link: "https://www.peterbilt.com/",
     },
-    
+
     {
         name: "RedBull",
-        logo: "/assets/sponsors/redbull.webp",
+        logo: "/assets/sponsors/redbull.png",
         link: "https://www.redbull.com/us-en/",
     },
 
@@ -96,61 +113,73 @@ export const SponsorData: Array<Sponsor> = [
         logo: "/assets/sponsors/rescale.svg",
         link: "https://www.rescale.com/",
     },
-    
+
     {
         name: "Rockwall MakerSpace",
-        logo: "/assets/sponsors/rockwallmakerspace.webp",
+        logo: "/assets/sponsors/rockwallmakerspace.png",
         link: "https://rockwallmakerspace.org/",
     },
-    
+
     {
         name: "SAE",
         logo: "/assets/sponsors/sae.svg",
         link: "https://www.sae.org/",
     },
-    
+
     {
         name: "Schroth Racing",
         logo: "/assets/sponsors/schroth.svg",
         link: "https://www.schrothracing.com/",
     },
-    
+
     {
         name: "Sherwin Williams",
         logo: "/assets/sponsors/sherwinwilliams.webp",
         link: "https://www.sherwin-williams.com/",
     },
-    
+
     {
         name: "Siemens",
         logo: "/assets/sponsors/siemens.png",
         link: "https://www.siemens.com/",
     },
-    
+
+    {
+        name: "Sigmatex",
+        logo: "/assets/sponsors/sigmatex.png",
+        link: "https://www.sigmatex.com/",
+    },
+
     {
         name: "SKF",
         logo: "/assets/sponsors/skf_logo.webp",
         link: "https://www.skf.com/us",
     },
-    
+
     {
         name: "SolidWorks",
         logo: "/assets/sponsors/solidworks.svg",
         link: "https://www.solidworks.com/",
     },
-    
+
     {
         name: "Tesla",
-        logo: "/assets/sponsors/tesla.webp",
+        logo: "/assets/sponsors/tesla-logo.png",
         link: "https://www.tesla.com/",
     },
-    
+
+    {
+        name: "teXtreme",
+        logo: "/assets/sponsors/teXtreme.png",
+        link: "https://www.textreme.com/",
+    },
+
     {
         name: "TTA",
         logo: "/assets/sponsors/tta.svg",
         link: "https://www.testtechnology.com/",
     },
-    
+
     {
         name: "UTD",
         logo: "/assets/sponsors/utd.svg",
@@ -161,6 +190,12 @@ export const SponsorData: Array<Sponsor> = [
         name: "WelshAdvisors",
         logo: "/assets/sponsors/welshadvisors.webp",
         link: "https://www.welshadvisors.com/",
+    },
+
+    {
+        name: "Woolf Aircraft",
+        logo: "/assets/sponsors/Woolf-Aircraft-logo.png",
+        link: "https://www.woolfaircraft.com/",
     },
 ];
 
