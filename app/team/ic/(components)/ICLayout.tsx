2e52d33164346a89fd5c972ee60d4ca097bc2289
Fernando4242 <portillofernando92@gmail.com>
2024-03-02 13:16:57 -0600

    added helper comment

--- app/team/ic/(components)/ICLayout.tsx
+++ app/team/ic/(components)/ICLayout.tsx
@@ -2,7 +2,10 @@ import { Config } from "../../../../types/ICPageConfig";
 
 export default function ICLayout({ config }: { config: Config }) {
     return (
-        <p>Hello World!</p>
+        <div>
+            {/* Individual components here for each section */}
+            Hello World!
+        </div>
     )
 }
 
