a051fbd5ed332bedd652c89e6131a6bfa3b89211
Fernando4242 <portillofernando92@gmail.com>
2024-04-29 23:35:31 -0500

    modified delay:

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -17,20 +17,20 @@ export default function Header() {
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-6 text-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.5 }}
+        transition={{ duration: 0.4, delay: 0.4 }}
       >
         <h1 className="text-5xl md:text-8xl font-bold text-wh-75 italic">
           <motion.span
             className="text-green-600 inline-block"
             animate={{ x: [-200, 0] }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.4, delay: 0.4 }}
           >
             DALLAS FORMULA
           </motion.span>{" "}
           <motion.span
             className="text-orange-600 inline-block"
             animate={{ x: [200, 0] }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.4, delay: 0.4 }}
           >
             RACING
           </motion.span>
