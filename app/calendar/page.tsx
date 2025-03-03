a7d69fa1ddd1987b950df4f66bd113b448f0aa20
seean7 <seandunn05@yahoo.com>
2025-03-02 20:34:14 -0600

    calendar navbar and the other changes for officer pages and got the pictures changed

---
+++ app/calendar/page.tsx
@@ -0,0 +1,9 @@
+import GoogleCalendarEmbed from "./Googlecal";
+
+export default function Page() {
+  return (
+    <main className="w-screen h-screen">
+      <GoogleCalendarEmbed />
+    </main>
+  );
+}
