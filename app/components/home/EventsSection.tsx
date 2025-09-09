8e81b3dc3f6706ba215ba59a5f0ed3baeecc68d5
Sean Dunn <seandunn05@yahoo.com>
2025-09-08 21:08:26 -0500

    All the updates dor a bit before kickoff number one

--- app/components/home/EventsSection.tsx
+++ app/components/home/EventsSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import reveal25 from "@/public/assets/events/revealDFR_25.png";
+import kickoff2025 from "@/public/assets/events/Kickoff2025fall.jpg";
 import cgd25 from "@/public/assets/events/cgd25.png";
 
 const EventsSection = () => {
@@ -19,33 +19,27 @@ const EventsSection = () => {
         className="shadow-lg rounded-2xl p-8 bg-gray-50"
       >
         <Image
-          src={reveal25}
+          src={kickoff2025}
           alt="Cars and Comets Event Banner"
           className="w-full max-w-3xl mx-auto h-auto object-contain rounded-lg mb-6"
           />
         <h2 className="text-4xl font-bold text-accent-orange mb-4">
-          The 2025 Unvailing is Here!!!
+          The 2025 Kickoff is Here!!!
         </h2>
         <p className="text-xl mb-2">
         It’s finally here. After months of late nights, tight deadlines, and relentless drive…
         
         </p>
         <p className="text-xl mb-2">
-        The 2025 IC/EV Dallas Formula Racing car is ready to be revealed.
+        The 2025 IC/EV Dallas Formula Racing team is ready for new members!
         </p>
 
         <ul className="text-lg mb-4">
-          <li><strong>📅Date:</strong> May 8, 2025</li>
-          <li><strong>🕕Time:</strong> 5:00 PM – 7:00 PM</li>
-          <li><strong>📍Location:</strong> UT Dallas – ECSW Axxess Atrium</li>
+          <li><strong>📅Date:</strong> September 10, 2025</li>
+          <li><strong>🕕Time:</strong> 7:00 PM – 9:00 PM</li>
+          <li><strong>📍Location:</strong> UT Dallas – ECSW 1.315</li>
         </ul>
-        <Link
-          href="https://forms.gle/XkhYYXiza4bAzXJUA"
-          target="_blank"
-          className="text-lg text-blue-600 underline"
-        >
-          🔗 Register Here
-        </Link>
+
         <p className="text-xl mt-4">We can’t wait to see you there!</p>
       </motion.div>
 
