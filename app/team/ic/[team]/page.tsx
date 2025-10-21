2e975bbe24d0b8edeb69279d1a07842e8be2d60a
Sean Dunn <seandunn05@yahoo.com>
2025-10-21 14:01:27 -0500

    hope

--- app/team/ic/[team]/page.tsx
+++ app/team/ic/[team]/page.tsx
@@ -2,24 +2,28 @@ import ICLayout from "../ICLayout";
 import TeamPageConfigs from "../../../../data/TeamPageConfigs.json";
 import { Config } from "../../../../types/ICPageConfig";
 
+
 interface Params {
-  params: {
-    team: string;
-  };
+ params: {
+   team: string;
+ };
 }
+
 
 interface TeamPageConfigsInterface {
-  [key: string]: Config | null;
+ [key: string]: Config | null;
 }
 
+
 export default function Page({ params: { team } }: Params) {
-  const config: Config | null =
-    (TeamPageConfigs as TeamPageConfigsInterface)[team] || null;
-
-  if (!config) {
-    return "Not Found";
-  }
+ const config: Config | null =
+   (TeamPageConfigs as TeamPageConfigsInterface)[team] || null;
 
 
-  return <ICLayout config={config} />;
+ if (!config) {
+   return "Not Found";
+ }
+
+
+ return <ICLayout config={config} />;
 }
--- next.config.js
+++ next.config.js
@@ -1,34 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+
 module.exports = {
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
-  ) => {
-    config.module.rules.push(
-      {
-        test: /\.(gltf)$/,
-        loader: "gltf-loader",
-        /**
-         * @type {import("gltf-loader").GLTFLoaderOptions}
-         */
-        options: {
-          uriResolver: (module) => {
-            let result = module.default ?? module;
-            // Use the "src" property returned by the `next-image-loader` if present:
-            if (typeof result === "object" && "src" in result)
-              result = result.src;
-            return result;
-          },
-        },
-      },
-      {
-        test: /\.(bin)$/,
-        type: "asset/resource",
-      },
-    );
-    return config;
-  },
+ webpack: (
+   config,
+   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+ ) => {
+   config.module.rules.push(
+     {
+       test: /\.(gltf)$/,
+       loader: "gltf-loader",
+       /**
+        * @type {import("gltf-loader").GLTFLoaderOptions}
+        */
+       options: {
+         uriResolver: (module) => {
+           let result = module.default ?? module;
+           // Use the "src" property returned by the `next-image-loader` if present:
+           if (typeof result === "object" && "src" in result)
+             result = result.src;
+           return result;
+         },
+       },
+     },
+     {
+       test: /\.(bin)$/,
+       type: "asset/resource",
+     },
+   );
+   return config;
+ },
+
 
 };
