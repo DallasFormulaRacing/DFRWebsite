18097c9284650bf5d19fbfbae1c3aa7e2b5919a7
prestonctx2006-rgb <prestonctx2006@gmail.com>
2025-10-28 13:17:24 -0500

    Removed Pickleball Tournament announcement

--- app/components/home/EventsSection.tsx
+++ app/components/home/EventsSection.tsx
@@ -5,47 +5,11 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Halloween from "@/public/assets/events/HalloweenParty.png";
-import Pickelball from "@/public/assets/events/Pickelball.png";
 import cgd25 from "@/public/assets/events/cgd25.png";
 
 const EventsSection = () => {
   return (
     <>
-    <section className="px-8 md:px-24 py-20 flex flex-col gap-16 bg-white">
-      
-
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        className="shadow-lg rounded-2xl p-0 bg-green-50 flex flex-row gap-8 place-items-center"
-      >
-        <Image
-          src={Pickelball}
-          alt="Cars and Comets Event Banner"
-          className="w-full max-w-3xl mx-auto h-auto object-contain rounded-lg"
-          />
-    <div className="p-8">
-        <h1 className="text-6xl font-bold text-accent-orange mb-4">
-          The UTD Pickelball Tournament is Here!!!
-        </h1>
-        <p className="text-xl mb-2">
-        Get ready to rally! Dallas Formula Racing is hitting the courts for a day of friendly competition, fast serves, and all-out fun. 
-        </p>
-        <p className="text-xl mb-2">
-        Whether you’re a seasoned player or new to the game, this tournament is all about teamwork, energy, and good vibes.        
-        </p>
-
-        <ul className="text-lg mb-4">
-          <li><strong>📅Date:</strong> 10/26/2025</li>
-          <li><strong>🕕Time:</strong>12:00PM - 4:00pm  </li>
-          <li><strong>📍Location:</strong>Club Studio Fitness</li>
-        </ul>
-
-        </div>
-      </motion.div>
-
-    </section>
     <section className="px-8 md:px-24 py-20 flex flex-col gap-16 bg-white">
       
 
